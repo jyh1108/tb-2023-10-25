0ae123cb158464ad446530925183dea6192f32c9
jyh1108 <kobcom12@naver.com>
2023-10-25 15:51:46 +0900

    1ë단계 ì 앱성공

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,13 +3,22 @@ package com.ll;
 import java.util.Scanner;
 
 class App {
-    void run() {
+    void un() {
+
         System.out.println("=== 명령 앱 ===");
+        while (true){
         System.out.print("명령) ");
 
         Scanner scanner = new Scanner(System.in);
         String cmd = scanner.nextLine();
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
+
+
+            if(cmd.equals("종료")){
+                break;
+            }
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+        }
+
     }
 }
