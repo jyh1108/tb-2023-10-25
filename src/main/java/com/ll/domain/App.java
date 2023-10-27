c7e68945e694f961be9b7fae5293a5a7796affa2
jyh1108 <kobcom12@naver.com>
2023-10-27 15:11:34 +0900

    롬복 사용

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -1,21 +1,21 @@
-package com.ll;
+package com.ll.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    Scanner scanner;
-    int lastQuotationId;
-    List<Quotation> quotations;
+    private Scanner scanner;
+    private int lastQuotationId;
+    private List<Quotation> quotations;
 
-    App() {
+    public App() {
         scanner = new Scanner(System.in);
         lastQuotationId = 0;
         quotations = new ArrayList<>();
     }
 
-    void run() {
+    public void run() {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -44,7 +44,7 @@ public class App {
         }
     }
 
-    void actionWrite() {
+    private void actionWrite() {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
 
@@ -60,7 +60,7 @@ public class App {
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
     }
 
-    void actionList() {
+    private void actionList() {
         System.out.println("번호 / 작가 / 명언");
 
         System.out.println("----------------------");
@@ -70,11 +70,11 @@ public class App {
 
         for (int i = quotations.size() - 1; i >= 0; i--) {
             Quotation quotation = quotations.get(i);
-            System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+            System.out.printf("%d / %s / %s\n", quotation.getId(), quotation.getAuthorName(), quotation.getContent());
         }
     }
 
-    void actionRemove(Rq rq) {
+    private void actionRemove(Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
         if (id == 0) {
@@ -82,10 +82,31 @@ public class App {
             return; // 함수를 끝낸다.
         }
 
-        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        int index = findQuotationIndexById(id);
+
+        if (index == -1) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+            return;
+        }
+
+        quotations.remove(index);
+
+        System.out.printf("%d번 명언을 삭제되었습니다.\n", id);
     }
 
-    void actionModify(Rq rq) {
+    private int findQuotationIndexById(int id) {
+        for (int i = 0; i < quotations.size(); i++) {
+            Quotation quotation = quotations.get(i);
+
+            if (quotation.getId() == id) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void actionModify(Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
         if (id == 0) {
@@ -93,6 +114,26 @@ public class App {
             return; // 함수를 끝낸다.
         }
 
-        System.out.printf("%d번 명언을 수정합니다.\n", id);
+        int index = findQuotationIndexById(id);
+
+        if (index == -1) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+            return;
+        }
+
+        Quotation quotation = quotations.get(index);
+
+        System.out.printf("명언(기존) : %s\n", quotation.getContent());
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.printf("작가(기존) : %s\n", quotation.getAuthorName());
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
+
+        quotation.setContent(content);
+        quotation.setAuthorName(authorName);
+
+        System.out.printf("%d번 명언을 수정되었습니다.\n", id);
     }
 }
---
+++ src/main/java/com/ll/domain/Quotation.java
@@ -0,0 +1,16 @@
+package com.ll.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@AllArgsConstructor
+public class Quotation {
+    @Getter
+    private int id;
+    @Getter
+    @Setter
+    private String content;
+    @Getter
+    @Setter
+    private String authorName;
+}
