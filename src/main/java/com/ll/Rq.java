b46d0f4799881c2f28788873b7cad39df6de238e
jyh1108 <kobcom12@naver.com>
2023-10-27 11:39:34 +0900

    23-10-27 맵 6단계

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,54 +1,54 @@
-package com.ll;
+    package com.ll;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class Rq {
-    String cmd;
-    String action;
-    String queryString;
-    List<String> paramNames;
-    List<String> paramValues;
+    public class Rq {
+        String cmd;
+        String action;
+        String queryString;
+        List<String> paramNames;
+        List<String> paramValues;
 
-    Rq(String cmd) {
-        paramNames = new ArrayList<>();
-        paramValues = new ArrayList<>();
+        Rq(String cmd) {
+            paramNames = new ArrayList<>();
+            paramValues = new ArrayList<>();
 
-        this.cmd = cmd;
+            this.cmd = cmd;
 
-        String[] cmdBits = cmd.split("\\?", 2);
-        action = cmdBits[0].trim();
-        queryString = cmdBits[1].trim();
+            String[] cmdBits = cmd.split("\\?", 2);
+            action = cmdBits[0].trim();
+            queryString = cmdBits[1].trim();
 
-        String[] queryStringBits = queryString.split("&");
+            String[] queryStringBits = queryString.split("&");
 
-        for (int i = 0; i < queryStringBits.length; i++) {
-            String queryParamStr = queryStringBits[i];
-            String[] queryParamStrBits = queryParamStr.split("=", 2);
+            for (int i = 0; i < queryStringBits.length; i++) {
+                String queryParamStr = queryStringBits[i];
+                String[] queryParamStrBits = queryParamStr.split("=", 2);
 
-            String paramName = queryParamStrBits[0];
-            String paramValue = queryParamStrBits[1];
+                String paramName = queryParamStrBits[0];
+                String paramValue = queryParamStrBits[1];
 
-            paramNames.add(paramName);
-            paramValues.add(paramValue);
+                paramNames.add(paramName);
+                paramValues.add(paramValue);
+            }
+        }
+
+        String getAction() {
+            return action;
+        }
+
+        public int getParamAsInt(String paramName, int defaultValue) {
+            int index = paramNames.indexOf(paramName);
+
+            if (index == -1) return defaultValue;
+
+            String paramValue = paramValues.get(index);
+
+            try {
+                return Integer.parseInt(paramValue);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
         }
     }
-
-    String getAction() {
-        return action;
-    }
-
-    public int getParamAsInt(String paramName, int defaultValue) {
-        int index = paramNames.indexOf(paramName);
-
-        if (index == -1) return defaultValue;
-
-        String paramValue = paramValues.get(index);
-
-        try {
-            return Integer.parseInt(paramValue);
-        } catch (NumberFormatException e) {
-            return defaultValue;
-        }
-    }
-}
